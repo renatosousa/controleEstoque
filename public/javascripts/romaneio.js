0b9cef0aa84785336977e91d2d309473499f796f
renatosousa <natocosta@gmail.com>
2015-06-18 13:57:51 -0300

    segundo commit

--- public/javascripts/romaneio.js
+++ public/javascripts/romaneio.js
@@ -2,26 +2,44 @@ var app = angular.module('appRomaneio', []);
 
 app.controller('ComprimentosController', function($scope) {
     $scope.itens = [
-        {comprimento: '3', quantidade: 3},
-        {comprimento: '3,5', quantidade: 2}
+        {comprimento: '3', quantidade: 0, indice: 0},
+        {comprimento: '3.5', quantidade: 0, indice: 1}
     ];
+
+    var valores = [$scope.itens[0].quantidade, $scope.itens[1].quantidade];
+    var contIndice = 2;
 
     $scope.adicionaComprimento = function () {
         $scope.itens.push({comprimento: $scope.item.comprimento,
-            quantidade: $scope.item.quantidade});
+            quantidade: $scope.item.quantidade,
+            indice: contIndice
+            });
         $scope.item.comprimento = $scope.item.quantidade = '';
+        contIndice++;
     };
 
 
-    $scope.$watchCollection('itens',function(newValue, oldValue) {
-        // Função disparada sempre que o objeto $scope.items sofrer alterações
-        $scope.total = 0;
-        angular.forEach($scope.itens, function(value, key) {
-            $scope.total += value.quantidade;
-        })
-    });
+    $scope.total = 0;
 
-    $scope.change = function() {
-        $scope.total;
+    $scope.excluir = function(indiceExcluir){
+        alert(indiceExcluir);
+        valores.splice(indiceExcluir,1);
+        $scope.itens.splice(indiceExcluir,1);
+        somarTotal();
+        contIndice--;
+    }
+
+    var somarTotal = function(){
+
+        $scope.total = 0;
+        angular.forEach(valores, function(value, key) {
+            $scope.total += value;
+        });
+
+    };
+
+    $scope.change = function(qt, indiceItem) {
+        valores[indiceItem] = qt;
+        somarTotal();
     };
 });
